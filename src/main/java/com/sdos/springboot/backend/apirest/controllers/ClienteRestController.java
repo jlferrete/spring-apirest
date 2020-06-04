6023c156530b3ad6b81d06db7de9d7df8c56b942
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-04 16:45:32 +0200

    Added Test Data

---
+++ src/main/java/com/sdos/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,25 @@
+package com.sdos.springboot.backend.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sdos.springboot.backend.apirest.models.entity.Cliente;
+import com.sdos.springboot.backend.apirest.models.services.IClienteService;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/clientes")
+	public List<Cliente> index() {
+		return clienteService.findAll();
+
+	}
+}
