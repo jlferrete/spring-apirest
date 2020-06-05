b27ded2d5b183323d59c6daa12530efd68974be0
jlferrete <joseluisferrete@gmail.com>
2020-06-05 19:23:15 +0200

    Added Show and Create methods in Controller

--- src/main/java/com/sdos/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/sdos/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,11 +1,17 @@
 package com.sdos.springboot.backend.apirest.controllers;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sdos.springboot.backend.apirest.models.entity.Cliente;
@@ -23,5 +29,16 @@ public class ClienteRestController {
 	public List<Cliente> index() {
 		return clienteService.findAll();
 
+	}
+	
+	@GetMapping("/clientes/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
 	}
 }
