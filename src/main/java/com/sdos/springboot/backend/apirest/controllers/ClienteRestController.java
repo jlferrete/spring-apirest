f777d4e3f852fce4be6eaee221b16e927d9f9846
jlferrete <joseluisferrete@gmail.com>
2020-06-07 18:49:54 +0200

    Added error messages on handler Delete

--- src/main/java/com/sdos/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/sdos/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -143,8 +143,20 @@ public class ClienteRestController {
 	}
 	
 	@DeleteMapping("/clientes/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable Long id) {
-		clienteService.delete(id);
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+		    clienteService.delete(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al eliminar el cliente de la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El cliente eliminado con éxito!");
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 	}
 }
