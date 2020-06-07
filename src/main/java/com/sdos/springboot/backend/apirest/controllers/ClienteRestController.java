ed2ac439ae591aa9783510ebb520a5eb9273ccfe
jlferrete <joseluisferrete@gmail.com>
2020-06-07 18:22:07 +0200

    Added error messages on handler show

--- src/main/java/com/sdos/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/sdos/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,10 +1,15 @@
 package com.sdos.springboot.backend.apirest.controllers;
 
 
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,8 +39,27 @@ public class ClienteRestController {
 	}
 	
 	@GetMapping("/clientes/{id}")
-	public Cliente show(@PathVariable Long id) {
-		return clienteService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+			
+			Cliente cliente = null;
+			Map<String, Object> response = new HashMap<>();
+			
+			try {
+				cliente = clienteService.findById(id);
+			} catch(DataAccessException e) {
+				response.put("mensaje", "Error al realizar la consulta en la base de datos");
+				response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			if(cliente == null) {
+				response.put("mensaje", "El cliente ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+			}
+			
+			return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+		
+		
 	}
 	
 	@PostMapping("/clientes")
