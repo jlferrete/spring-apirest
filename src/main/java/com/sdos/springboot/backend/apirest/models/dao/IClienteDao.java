8a1ee34ef208feb2f424182670628ad85c564e65
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-10 14:25:36 +0200

    Added new entity Region

--- src/main/java/com/sdos/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/sdos/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,10 +1,15 @@
 package com.sdos.springboot.backend.apirest.models.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sdos.springboot.backend.apirest.models.entity.Cliente;
+import com.sdos.springboot.backend.apirest.models.entity.Region;
 
 public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
+	@Query("from Region")
+	public List<Region> findAllRegiones();
 }
