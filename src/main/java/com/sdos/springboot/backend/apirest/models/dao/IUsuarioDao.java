a42a84088f2e85479dfbde16b9e6ede592509b7d
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-12 07:47:42 +0200

    Added JPA IUsuarioDAO Repository

---
+++ src/main/java/com/sdos/springboot/backend/apirest/models/dao/IUsuarioDao.java
@@ -0,0 +1,16 @@
+package com.sdos.springboot.backend.apirest.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.sdos.springboot.backend.apirest.models.entity.Usuario;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long> {
+
+	public Usuario findByUsername(String username);
+	
+	@Query("select u from Usuario u where u.username=?1")
+	public Usuario findByUsername2(String username);
+	
+	
+}
