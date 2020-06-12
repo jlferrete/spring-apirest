5fe0826ce85a20b93f70186129c89a7cc226a323
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-12 09:41:39 +0200

    Added configuration for server authorization

--- src/main/java/com/sdos/springboot/backend/apirest/auth/SpringSecurityConfig.java
+++ src/main/java/com/sdos/springboot/backend/apirest/auth/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.sdos.springboot.backend.apirest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,6 +25,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
 	}
+
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
+	
+	
 	
 	
 }
