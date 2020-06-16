7d64fd3ecc456ed88270c95bc76a5d8d0e0055f5
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-16 08:20:04 +0200

    Added relations ManyToOne 2Ways Entities Factura & Clientes

--- src/main/java/com/sdos/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/sdos/springboot/backend/apirest/models/entity/Cliente.java
@@ -1,8 +1,11 @@
 package com.sdos.springboot.backend.apirest.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -55,7 +59,14 @@ public class Cliente implements Serializable {
 	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 	private Region region;
 	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
+	private List<Factura> facturas;
 	
+		
+	public Cliente() {
+		this.facturas = new ArrayList<>();
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -111,6 +122,17 @@ public class Cliente implements Serializable {
 	public void setRegion(Region region) {
 		this.region = region;
 	}
+	
+	
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+
 
 	private static final long serialVersionUID = 1L;
 }
---
+++ src/main/java/com/sdos/springboot/backend/apirest/models/entity/Factura.java
@@ -0,0 +1,86 @@
+package com.sdos.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="facturas")
+public class Factura implements Serializable{
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descripcion;
+	
+	private String observacion;
+	
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Cliente cliente;
+	
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getObservacion() {
+		return observacion;
+	}
+
+	public void setObservacion(String observacion) {
+		this.observacion = observacion;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+}
